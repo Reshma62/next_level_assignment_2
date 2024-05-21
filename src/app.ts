d162ed0159c8208861ce091db0477220957194b5
Reshma62 <mernstack2024@gmail.com>
2024-05-21 22:30:12 +0600

    try to deploy vercel

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { ProductRoute } from "./app/modules/products/product.route";
 import { OrderRoute } from "./app/modules/orders/order.route";
@@ -8,7 +8,12 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", ProductRoute);
 app.use("/api", OrderRoute);
-
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({
+    success: false,
+    message: "Route not found",
+  });
+});
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
--- src/app/modules/orders/order.service.ts
+++ src/app/modules/orders/order.service.ts
@@ -1,14 +1,33 @@
+import { IInventory } from "../products/product.interface";
 import Product from "../products/product.model";
 import { IOrder } from "./order.interface";
 import Order from "./order.model";
 
 export const createOrderService = async (order: IOrder) => {
-  const { productId } = order;
+  const { productId, quantity } = order;
   const existsProduct = await Product.findOne({ _id: productId });
   if (!existsProduct) {
     throw new Error("Product not found");
   }
+
+  if ((existsProduct.inventory as IInventory).quantity === 0) {
+    await Product.findOneAndUpdate(
+      { _id: productId },
+      { $set: { "inventory.inStock": false } },
+      { new: true }
+    );
+
+    throw new Error("Out of stock");
+  }
+  if ((existsProduct.inventory as IInventory).quantity < quantity) {
+    throw new Error("Insufficient quantity available in inventory");
+  }
   const result = await Order.create(order);
+  await Product.findOneAndUpdate(
+    { _id: productId },
+    { $inc: { "inventory.quantity": -quantity } },
+    { new: true }
+  );
   return result;
 };
 
@@ -16,5 +35,8 @@ export const createOrderService = async (order: IOrder) => {
 export const getAllOrdersService = async (query: Partial<IOrder>) => {
   // Use the find method with the query object
   const result = await Order.find(query).exec();
+  if (result.length === 0) {
+    throw new Error("Order not found");
+  }
   return result;
 };
